4e73e7ec48ef25d2dff0376d843c935ef2454d66
yunosuke01 <yuu1m5+chale@gmail.com>
2018-11-19 20:37:09 +0900

    変更

--- www/js/test.js
+++ www/js/test.js
@@ -1,2 +1,14 @@
 // This is a JavaScript file
-
+ons.bootstrap()
+      .controller('AppController', function() {
+        var self = this;
+        
+        self.init = function() {
+          setTimeout(function() {
+            app.navi.pushPage('top.html');
+          }, 2000);
+        }
+      });
+    ons.ready(function() {
+        console.log("Onsen UI is ready!");
+    });
